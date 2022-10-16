a0d9edc5e987878af0287e2a0690a36684c31cdd
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-10-16 16:42:16 +0500

    skeleton

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,5 +1,18 @@
-
-pub struct Neuron {
+struct Neuron {
     weights: Vec<f32>,
     bias: f32
+}
+
+struct Layer {
+    neurons: Vec<Neuron>
+}
+
+pub struct Network {
+    layers: Vec<Layer>
+}
+
+impl Network {
+    pub fn propagate(inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
+    }
 }
