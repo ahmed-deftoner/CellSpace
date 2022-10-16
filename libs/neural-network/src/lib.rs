f58e737c3124938477eea7f4eb2dd898dfee335d
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-10-16 22:34:29 +0500

    network random

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -11,6 +11,10 @@ pub struct Network {
     layers: Vec<Layer>
 }
 
+pub struct LayerTopology {
+    pub neurons: usize,
+}
+
 impl Neuron {
     fn propagate(&self, inputs: &[f32]) -> f32 {
         let output = inputs.iter()
@@ -35,5 +39,22 @@ impl Network {
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+    
+    pub fn random(layers: Vec<LayerTopology>) -> Self {
+        assert!(layers.len() > 1);
+        let mut built_layers = Vec::new();
+
+        for i in 0..(layers.len() - 1) {
+            let input_neurons = layers[i].neurons;
+            let output_neurons = layers[i + 1].neurons;
+
+            built_layers.push(Layer::random(
+                input_neurons,
+                output_neurons,
+            ));
+        }
+
+        Self { layers: built_layers }
     }
 }
